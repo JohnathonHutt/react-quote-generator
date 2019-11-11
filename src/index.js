0b79bc797d64d3d02ffafbd32507bc07124aafc8
Johnathon Hutt <johnathonhutt@gmail.com>
2019-11-11 16:09:52 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "",
+      author: ""
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    this.updateQuote = this.updateQuote.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://api.quotable.io/random')
+      .then(response => response.json())
+      .then(data => {
+      this.setState({
+        quote: data.content,
+        author: data.author
+      });
+    })
+  }
+
+  updateQuote() {
+    fetch('https://api.quotable.io/random')
+      .then(response => response.json())
+      .then(data => {
+      this.setState({
+        quote: data.content,
+        author: data.author
+      });
+    })
+  }
+
+  render() {
+    let encodeQuote = encodeURI(this.state.quote);
+    let encodeAuthor = encodeURI(this.state.author);
+    let href = "https://twitter.com/intent/tweet?text=" + encodeQuote + " -" + encodeAuthor;
+    return (
+      <div id="quote-box">
+        <h1>Quotes</h1>
+        <div className="quote-and-author">
+          <p id="text">{this.state.quote}</p>
+          <p id="author"> - {this.state.author}</p>
+        </div>
+        <button id="new-quote" className="btn quote-button" onClick={this.updateQuote}>Quote</button>
+        <a className="twitter-share-button btn tweet" href={href} data-size="large">
+          Tweet
+        </a>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
